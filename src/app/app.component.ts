d41278441d288dd4c245ffd3848f117b6acca248
LakshaySharma01 <laqsh001@gmail.com>
2025-05-05 14:55:19 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormComponent } from './FormComponent/form/form.component'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

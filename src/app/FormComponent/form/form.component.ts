f6c87fa705e42137312703bc5f8789777bb3883b
LakshaySharma01 <laqsh001@gmail.com>
2025-05-06 11:33:00 +0530

    Added registration changes

--- src/app/FormComponent/form/form.component.ts
+++ src/app/FormComponent/form/form.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css'] 
+  styleUrls: ['./form.component.css'], 
+  imports:[ReactiveFormsModule]
 })
-export class FormComponent {}
+export class FormComponent { 
+  registrationForm = new FormGroup({
+    fullName: new FormControl('',Validators.required),
+    lastName: new FormControl(''),
+  })
+
+  public registrationFormSubmit(){
+    console.log(this.registrationForm.value)
+  }
+}

213d6ffceb7a60285c39b0ec25d2606c726c01e1
LakshaySharma01 <laqsh001@gmail.com>
2025-05-07 17:03:48 +0530

    Added Weather Application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { LandComponent } from "./LandingPage/land/land.component";
-import {UsersDataService} from './services/users-data.service';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,21 @@
-import { Routes , RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { FormComponent } from './FormComponent/form/form.component';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-
+import { WeatherComponent } from './weather/weather.component';
 
 export const routes: Routes = [
-    { path: 'register', component: FormComponent },
-    {path: 'login' , component:LoginComponent},
-   { path: '' , component: HomeComponent}
+  { path: 'register', component: FormComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'weather', component: WeatherComponent },
+  { path: '', component: HomeComponent },
+];
 
-  ];
-
-  @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
 
 // export const routes: Routes = [];
-export class AppRoutingModule { }
+export class AppRoutingModule {}
